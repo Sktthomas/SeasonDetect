0455f4f01e8ad4b222ac3f0697c50bfeae1cafad
Thomas Nielse <skt.thomas@gmail.com>
2020-02-05 11:53:04 +0100

    Added setState function that takes the latitude of the position object

--- src/index.js
+++ src/index.js
@@ -7,19 +7,21 @@ class App extends React.Component { //Class component instead of functional comp
     constructor(props) {
         super(props); //Since we're inheriting from React.Component, we have to set up the constructor code using the super() to first construct a React Component
 
-        this.state = { lat: null } //state object. Will contain relevant data like our latitude. We initialise it as null since we don't know it yet
+        this.state = { lat: null } //state object. Will contain relevant data like our latitude. We initialise it as null since we don't know it yet. This is the only time we assign this.state using "="
 
+        //since this will only be called once when the component is initialized, we would be better off putting it here, rather than in the render, which gets called all the time as updates are made
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState( { lat: position.coords.latitude }); //Sets state to position latitude USING SETSTATE
+            }, //Success callback gets us a position object
+            (error) => console.log(error) //failure callback gives us an error object
+        );
     }
 
     //React says: We have to define render(). If we don't we get an error
     render() { //class has one method by default. Therefore we also needs to implement React.component. We are subclassing it into App class
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position), //Success callback gets us a position object
-            (error) => console.log(error) //failure callback gives us an error object
-        );
-
-        return <div>{this.state.lat} </div>
+        return <div>Latitude: {this.state.lat} </div>
     }
 }
 
