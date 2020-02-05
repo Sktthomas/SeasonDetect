63b656750dcdcc9aeda50676bff0443f5aa7c0e0
Thomas Nielse <skt.thomas@gmail.com>
2020-02-05 14:25:02 +0100

    Inserts text depending on whether user is in summer or winter, using ternary expressions. Also added icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
-       return lat > 0 ? 'summer' : 'winter';
+       return lat > 0 ? 'summer' : 'winter'; //Ternary expression that goes: 'Is latitude greater than 0? If so it's summer, otherwise it's winter
     } else {
         return lat > 0 ? 'winter' : 'summer';
     }
@@ -11,10 +11,17 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => { //Remember to set props as arguments to use it!
 
     const season = getSeason(props.lat, new Date().getMonth());
+    const seasonText = season === 'winter' ? 'Brrr it is chilly!' : 'Let\'s hit the beach!'
+    const seasonIcon = season === 'winter' ? 'snowflake' : 'sun';
 
-    console.log(season)
-
-    return <div>Season Display</div>
+return( 
+    <div>
+        <i className={`${seasonIcon} icon`}/> {/* Backtick expression that inserts the result of the seasonIcon helper function as a string */}
+        <h1>{seasonText}
+        </h1>
+        <i className={`${seasonIcon} icon`}/>
+    </div>
+);
 }
 
 export default SeasonDisplay
